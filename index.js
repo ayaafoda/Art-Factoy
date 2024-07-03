6eab780dd18a0a852bc6a06e2559f511bafdefe6
aya <ayafoda69@gmail.com>
2024-07-03 15:51:30 +0300

    Via Files

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let bars = document.querySelector(".lins");
 let li = document.querySelector(".second");
 let subenu = document.querySelector(".submanu");
 
+let list = Array.from(document.querySelectorAll(".item-list .item"));
+console.log(list)
+
 bars.onclick = () => {
   ul.classList.toggle("open");
 };
@@ -11,37 +14,40 @@ li.onclick = () => {
   subenu.classList.toggle("open");
 };
 
-// window.onscroll = () => {
-//   let img = document.querySelector(".first .container .image img");
-//   if (window.scrollY >= img.offsetTop) {
-//     img.style.left = "0";
-//     img.style.position = "relative";
-//     img.style.opacity = "1";
-//   } else if (window.scrollY <= img.offsetTop) {
-//     // img.style.left = "-105%";
-//   }
-// };
-
 window.onscroll = () => {
-  if (window.scrollY >= 500) {
-    document.querySelector("header").style.position = "sticky";
-    document.querySelector("header").style.backgroundColor = "#fff";
-    document.querySelector("header").style.boxShadow =
-      "black 0px -4px 20px 0px";
-    document.querySelector("h1 a").style.color = "black";
-  } else if (window.scrollY <= 400) {
-    document.querySelector("h1 a").style.color = "white";
-    document.querySelector("header").style.boxShadow = "none";
-    document.querySelector("header").style.backgroundColor =
-      "rgba(255 255 255 / 0)";
-    document.querySelector("header").style.position = "relative";
-  }
+  header();
+  let two = document.querySelector(".second");
+  if (window.scrollY >= two.scrollHeight) {
 
+    document.querySelector(".img1").style.right = "0";
+    document.querySelector(".img1").style.opacity = "1";
+  }
+  let container1 = document.querySelector(".first .one");
+  let img1 = document.querySelector(".first .container img");
+  if (window.scrollY >= container1.scrollHeight) {
+    console.log(container1.scrollHeight);
+    img1.style.left = "0";
+    img1.style.opacity = "1";
+  }
+};
+
+function header() {
+  if (window.scrollY >= 200) {
+    document.querySelector("header").classList.add("trans-header");
+    document.querySelector("h1 a").style.color = "black";
+  } else if (window.scrollY <= 200) {
+    document.querySelector("h1 a").style.color = "white";
+    document.querySelector("header").classList.remove("trans-header");
+  }
+  headersElements();
+}
+
+function headersElements() {
   // loop on bars
   for (let i = 0; i < document.querySelectorAll(".span").length; i++) {
-    if (window.scrollY >= 500) {
+    if (window.scrollY >= 200) {
       document.querySelectorAll(".span")[i].style.backgroundColor = "black";
-    } else if (window.scrollY <= 300) {
+    } else if (window.scrollY <= 200) {
       document.querySelectorAll(".span")[i].style.backgroundColor = "white";
     }
   }
@@ -50,31 +56,22 @@ window.onscroll = () => {
   for (let i = 0; i < document.getElementsByClassName("link").length; i++) {
     if (
       document.querySelector("header").offsetWidth <= 990 ||
-      window.scrollY > 400
+      window.scrollY > 200
     ) {
       document.getElementsByClassName("link")[i].style.color = "black";
     } else if (
       document.querySelector("header").offsetWidth > 900 &&
-      window.scrollY < 400
+      window.scrollY < 200
     ) {
       document.getElementsByClassName("link")[i].style.color = "white";
     }
   }
+}
 
-  if (window.scrollY >= document.querySelector(".second ").offsetTop) {
-    document.querySelector(".second .container .image img").style.right = "0";
-    document.querySelector(".second .container .image img").style.position ="relative";
-    document.querySelector(".second .container .image img").style.opacity =
-      "1";
-  }
-  let img1 = document.querySelector(".first .container");
-  let img = document.querySelector(".first .container .image img");
-  if (window.scrollY >= img1.offsetTop) {
-    img.style.left = "0";
-    img.style.position = "relative";
-    img.style.opacity = "1";
-  }
-};
 
-// console.log(document.querySelector("header").offsetWidth);
-// console.log(window.scrollY);
+
+
+document.querySelector(".prev-btn").onclick = () => {
+  list.style.right = "25px";
+}
+
